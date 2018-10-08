a3bb2f0ee77ea1668e0d8531ccfc1deca4ced18d
Richm <ric5hme45@yahoo.com>
2018-10-08 11:48:50 +0300

    get1

--- python/GET1.py
+++ python/GET1.py
@@ -2,12 +2,14 @@
 # Foundations of Python Network Programming - Chapter 9 -
 # using the 'verbose_handler.py' to prints requests and responses.
 #
-
-url ='http://www.ietf.org/rfc/rfc2616.txt'
+url = 'http://www.bbce.com/'
+#url ='http://www.ietf.org/rfc/rfc2616.txt'
 from verbose_handler import VerboseHTTPHandler
 import urllib, urllib2
 opener = urllib2.build_opener(VerboseHTTPHandler)
 info = opener.open(url)# open and print verbose massages
+#In case the URL does not exist the header of 404 message is printed
+# and after this hader a python error message and Exit 1
 code = info.code
 print("Code = '%s'." % code)
 msg = info.msg
@@ -17,3 +19,11 @@ keys = sorted(info.headers.keys())# key is alist of key headers
 
 print (keys)
 
+print("File contant")
+
+#bring the entire data stream into memory as a single string:
+print (info.read().strip())
+
+#you can also  read the info object in pieces through read(N)
+#  or readline();
+#line  146
--- python/verbose_handler.py
+++ python/verbose_handler.py
@@ -16,16 +16,16 @@ import StringIO, httplib, urllib2
 class VerboseHTTPResponse(httplib.HTTPResponse):
  def _read_status(self):
     s = self.fp.read()
-    print '-' * 20, 'Response', '-' * 20
-    print s.split('\r\n\r\n')[0]
+    print ('-' * 20, 'Response', '-' * 20)
+    print (s.split('\r\n\r\n')[0])
     self.fp = StringIO.StringIO(s)
     return httplib.HTTPResponse._read_status(self)
 
 class VerboseHTTPConnection(httplib.HTTPConnection):
     response_class = VerboseHTTPResponse
     def send(self, s):
-      print '-' * 50
-      print s.strip()
+      print ('-' * 50)
+      print (s.strip())
       httplib.HTTPConnection.send(self, s)
 
 class VerboseHTTPHandler(urllib2.HTTPHandler):
