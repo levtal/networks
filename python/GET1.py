4886f3316ec73781177aa033e24e9fb8a31a6357
Richm <ric5hme45@yahoo.com>
2018-10-09 11:24:48 +0300

    get1

--- python/GET1.py
+++ python/GET1.py
@@ -1,15 +1,27 @@
 #!/usr/bin/env python
 # Foundations of Python Network Programming - Chapter 9 -
 # using the 'verbose_handler.py' to prints requests and responses.
-#
-url = 'http://www.bbce.com/'
-#url ='http://www.ietf.org/rfc/rfc2616.txt'
+# url = 'http://example.com/better-living-through-http'
+url ='http://www.bbc.com'
 from verbose_handler import VerboseHTTPHandler
 import urllib, urllib2
 opener = urllib2.build_opener(VerboseHTTPHandler)
-info = opener.open(url)# open and print verbose massages
+#info = opener.open(url)# open and print verbose massages
 #In case the URL does not exist the header of 404 message is printed
-# and after this hader a python error message and Exit 1
+# and after this header a python error message and Exit 1
+
+try:
+    info = opener.open(url)
+except urllib2.HTTPError, e:
+    error_code = e.code
+    print("Error Code = '%s'." % error_code)
+    error_msg = e.msg
+    print("Error msg = '%s'." % error_msg)
+    print("Error File content")
+
+    # bring the entire data stream into memory as a single string:
+    print (e.read().strip())
+
 code = info.code
 print("Code = '%s'." % code)
 msg = info.msg
@@ -26,4 +38,4 @@ print (info.read().strip())
 
 #you can also  read the info object in pieces through read(N)
 #  or readline();
-#line  146
+#line  147
